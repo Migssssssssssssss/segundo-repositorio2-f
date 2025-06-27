06250fddf2babe84021d6f1216e7616571ced85b
Migssssssssssssss <miguel.eduardo.almeida@escola.pr.gov.br>
2025-06-26 21:07:26 -0300

    26/06/25 (1)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave); {
-    alert("Fui clicado!")
+botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+
+function mostraPalavraChave() {
+    alert("Fui clicado!");
 }
